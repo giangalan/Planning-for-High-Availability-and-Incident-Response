06ef8768a9a96d444247681ebb5482d5d242847d
Phan Minh Giang <ganalanphan@gmail.com>
2024-09-09 12:10:30 +0700

    Fix alb vpc, subnet for zone 2

--- project/starter-code/zone2/alb.tf
+++ project/starter-code/zone2/alb.tf
@@ -3,6 +3,6 @@ module "project_alb" {
   source    = "./modules/alb"
   ec2       = module.project_ec2.aws_instance
   ec2_sg    = module.project_ec2.ec2_sg
-  subnet_id = ["subnet-0c0102a5423647adc", "subnet-0cac62965e1eddd26"]
-  vpc_id    = "vpc-067e80a926124cbf5"
+  subnet_id = ["subnet-0ee6710e62155a433", "subnet-0370af9d1fa892067"]
+  vpc_id    = "vpc-004fe25a1e94dfa60"
 }
